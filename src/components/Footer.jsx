3f5b6dc89812fa339a35331cf70507daa30adb88
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-08 21:51:02 +0200

    footer added and styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="h-16 border-t py-4 text-center mt-7">
+      <p className="text-lg text-custom-raisin-black">{`SoundMonk ${year}`}</p>
+    </footer>
+  );
 };
 
 export default Footer;
