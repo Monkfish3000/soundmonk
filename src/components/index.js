64dfb79897ac771ef5303d32eb39b568fee00d0d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-08 21:11:19 +0200

    hero styled

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import CardShowcase from './CardShowcase';
 
-export { Footer, Navbar, Hero };
+export { Footer, Navbar, Hero, CardShowcase };
