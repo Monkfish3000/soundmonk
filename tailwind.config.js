7285712f87359d0b21570af3543516ddf8ab9e8e
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-08 20:48:55 +0200

    navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-purple': '#3b2d4c',
+        'custom-purple-light': '#F4EFFB',
+        'custom-purple-medium': '#B6A5CA',
+        'custom-raisin-black': '#140F1A',
+      },
+    },
   },
   plugins: [],
-}
+};
